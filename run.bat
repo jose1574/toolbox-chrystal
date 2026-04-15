59b8616a86e0bcb42809d697eea19a3a8dacd7a5
jose1574 <jose.gs1574@gmail.com>
2026-04-15 07:31:49 -0400

    se crea el archivo run.bat para facilitar el arranque de la aplicacion

---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+:: 1. Cambiar el directorio a la ubicación donde reside este archivo .bat
+cd /d "%~dp0"
+
+:: 2. Ejecutar usando la ruta relativa a esta carpeta
+".\venv\Scripts\python.exe" app.py
